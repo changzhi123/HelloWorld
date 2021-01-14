7eafc0103a5e57a07bef2b67d2d54fe81995d6ea
changzhi <156230555@qq.com>
2021-01-14 18:42:08 +0800

    fate:新增编辑框

--- src/views/journal/MovableDecorator/components/popup.vue
+++ src/views/journal/MovableDecorator/components/popup.vue
@@ -49,6 +49,7 @@ export default {
     top: 0;
     text-align: center;
     // border: solid #ccc 1px;
+    box-shadow: 0 1px 6px 0 rgba(0,0,0,.25);
     background: #fff;
     .tab{
         width: 100%;
