1794d9e17281773930649be509a61b06ffda7940
changzhi <156230555@qq.com>
2021-02-07 11:39:42 +0800

    fate:更新注释

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,13 @@ export default {
       ${process.env.ENV}  - ${process.env.VUE_APP_URL}-`)
   },
   methods:{
-    
+    /*
+    document.visibilityState的值为：
+    hidden（当浏览器最小化、切换tab、电脑锁屏时）、
+    visible（用户正在查看当前页面时）、
+    prerender（文档加载离屏或者不可见）、
+    unloaded（当文档将要被unload时）。
+    */
      // 开启切换选项卡操作 /监听浏览器窗口选项卡的切换
       startVisibilitychange(){
         console.log('startVisibilitychange')
