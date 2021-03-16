740135e5cebc2bf246bc85dff895c2e665537af8
Your Name <156230555@qq.com>
2021-03-16 20:50:53 +0800

    fate:更新

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,11 +1,21 @@
 <template>
-  <div>user</div>
+  <div>user
+
+    <h2>{{state.count}}</h2>
+    <button>按钮</button>
+  </div>
 </template>
 
-<script>
-export default {
+<script setup>
+import { defineProps, reactive } from "vue";
 
+defineProps({
+});
+function get(){
+  xhr.open('get','')
+  xhr.send()
 }
+const state = reactive({ count: 123 });
 </script>
 
 <style>
