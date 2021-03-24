e4a6dade3189656f4e42a9b17c532256d3892d8c
unknown <156230555@qq.com>
2021-03-25 00:07:29 +0800

    fate:更新

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
-  <!-- <div class="rootBody"> -->
-    <a-config-provider :locale="state.locale">
-      <router-view />
+<a-config-provider :locale="state.locale"> 
+   <router-view />
     </a-config-provider>
-  <!-- </div> -->
 </template>
 
 <script setup>
@@ -14,55 +12,8 @@ const state = reactive({
   locale: zhCN,
 })
 import {reminder,formatDate}from '@/utils/index.js'
-// formatDate(new Date().getTime(),'YYYY-MM-DD HH:mm')
-// reminder('错误',{type:'error',duration:2,onClose:()=>{
-//   console.log(123456)
-// }})
-// import {
-//   // ref,
-//   toRefs,
-//   reactive,
-//   // computed,
-//   onMounted,
-//   // onUpdated,
-//   // onUnmounted,
-//   // onRenderTracked,
-//   // onRenderTriggered,
-//   // defineComponent
-// } from 'vue'
-// import { useStore } from 'vuex'
-// import { useRouter, useRoute } from 'vue-router'
-// import { projectName } from '@/utils/keyName.js'
-// import { getCurrentInstance,toRaw } from 'vue'
-// export default {
-//   name: 'App',
-//   components: {
-//     AppHeader,
-//   },
-//   setup(props, res) {
-//       const { ctx } = getCurrentInstance()
-//     const router = useRouter()
-//     const route = useRoute()
-//     const store = useStore()
-//     const state = reactive({})
-//     console.log(router,route,'路由信息',ctx.$router.currentRoute.value,route.path,)
-//     onMounted(() => {
-//       const modules = import.meta.glob('./assets/images/*.jpg')
-//       const modulesaa = import.meta.globEager('./assets/images/*.jpg')
-//       console.log(modules, 'modules',modulesaa)
-
-//       Object.keys(modules).filter((key) => {
-//         console.log(modules[key]) // 获取到属性对应的值，做一些处理
-//       })
-//     })
-//     return {
-//       ...toRefs(state),
-//       // count: computed(() => store.state.count),
-//       // increment: () => store.commit('increment'),
-//       // asyncIncrement: () => store.dispatch('asyncIncrement')
-//     }
-//   },
-// }
 </script>
-<style lang="less" scoped>
+
+<style>
+#app {}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
-// main.js
-
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// createApp(App).mount('#app')
 
 import router from './router'//路由
 import store from './store'//vuex
@@ -12,15 +11,13 @@ import 'ant-design-vue/dist/antd.css';//ui组件样式
 
 import * as Icons from "@ant-design/icons-vue";
 
-const app = createApp(App)
+const appRoot = createApp(App)
     .use(router)
     .use(store)
     .use(Antd)
-app.mount('#app')
 
+appRoot.mount('#app')
 
 Object.keys(Icons).filter(key=>{
-    app.component(key, Icons[key]);//全局引入icon图标
+    appRoot.component(key, Icons[key]);//全局引入icon图标
 })
-
-
